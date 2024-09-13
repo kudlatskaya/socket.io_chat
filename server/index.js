d2d2314c99c04428a8dd86912a06770e620306fe
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2024-09-13 13:24:30 +0300

    route created

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+const app = express();
+
+const route = require('./route')
+app.use(cors({ origin: "*" }));
+app.use(route)
+
+const server = http.createServer(app);
+
+server.listen(5000, () => {
+    console.log('Server is running');
+});
