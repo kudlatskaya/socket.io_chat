488ea7e904176d2e612394c76fa1d2a98c56ebf8
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2024-09-18 14:56:28 +0300

    form completed

--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -1,7 +1,26 @@
 import s from '../styles/Main.module.css'
 import { Link } from 'react-router-dom'
+import {useState} from "react";
+
+const FIELDS = {
+    NAME: 'name',
+    ROOM: 'room',
+}
 
 const Main = () => {
+    const { NAME, ROOM } = FIELDS;
+
+    const [values, setValues] = useState({ [NAME]: "", [ROOM]: "" })
+
+    const handleChange = ({ target: { value, name } }) => {
+        setValues({ ...values,  [name]: value})
+    }
+
+    const handleClick = (e) => {
+        const isDisabled = Object.values(values).some((value) => !value);
+        console.log(isDisabled)
+    }
+
     return (
         <div className={s.wrap}>
             <div className={s.container}>
@@ -9,15 +28,30 @@ const Main = () => {
 
                 <form className={s.form}></form>
                 <div className={s.group}>
-                    <input type="text" placeholder='username' name="username" value="" className={s.input} onChange={() => {
-                    }} autoComplete="off"/>
+                    <input type="text"
+                           placeholder='Username'
+                           name="name" value={values[NAME]}
+                           className={s.input}
+                           onChange={ handleChange}
+                           autoComplete="off"
+                           required/>
                 </div>
                 <div className={s.group}>
-                    <input type="text" name="room" placeholder='Room' value="" className={s.input} onChange={() => {
-                    }} autoComplete="off"/>
+                    <input type="text"
+                           name="room"
+                           placeholder='Room'
+                           value={values[ROOM]}
+                           className={s.input}
+                           onChange={handleChange}
+                           autoComplete="off"
+                           required/>
                 </div>
-                <Link to='/chat' >
-                    <button type="submit"></button>
+                <Link className={s.group}
+                      onClick={handleClick}
+                      to={`/chat?name=${values[NAME]}`} >
+                    <button type="submit" className={s.button} >
+                        Sign in
+                    </button>
                 </Link>
             </div>
         </div>
